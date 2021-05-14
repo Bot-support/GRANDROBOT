795a6abef7aff74d5dd4bc2697230bc13f05df87
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-05-14 22:56:16 +0530

    Update rules.py

--- tg_bot/modules/rules.py
+++ tg_bot/modules/rules.py
@@ -100,7 +100,7 @@ __help__ = """
  - /clearrules: clear the rules for this chat.
 """
 
-__mod_name__ = "Rules®️"
+__mod_name__ = "🔥Rules🔥"
 
 GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
 SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
