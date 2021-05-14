a6f9df0b46dbe56e8f0f2a1cfce191eee2d50663
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-05-14 21:10:24 +0530

    Update backups.py

--- tg_bot/modules/backups.py
+++ tg_bot/modules/backups.py
@@ -78,5 +78,5 @@ EXPORT_HANDLER = CommandHandler("export", export_data)
 dispatcher.add_handler(IMPORT_HANDLER)
 dispatcher.add_handler(EXPORT_HANDLER)
 
-__mod_name__ = "BACKUP📦"
+__mod_name__ = "Backup🔥"
 __handlers__ = [IMPORT_HANDLER, EXPORT_HANDLER]
